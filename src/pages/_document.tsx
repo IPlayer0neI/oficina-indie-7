d8e5cd34e4e94a0b1364cbe436a1526ff21ea97f
Gabriel <101530455+IPlayer0neI@users.noreply.github.com>
2025-04-24 22:08:40 -0300

    Fixes viewport width issue on mobile devices. Updates the iewport meta tag to user-scalable=no.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head />
       <title>Oficina Indie 7</title>
       <link rel="icon" href="/icon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7, user-scalable=no" />
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
       <body className="has-scrollbar">
         <Main />
